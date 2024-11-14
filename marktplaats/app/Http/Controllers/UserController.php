6c8acf0c71c390b6ef71f74c7f6b3f298cf2cc17
elsem1 <ikke_elwin@hotmail.com>
2024-11-14 15:56:35 +0100

    started with layout

--- marktplaats/app/Http/Controllers/UserController.php
+++ marktplaats/app/Http/Controllers/UserController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        if (!Auth::check())
+        {
+            return view('auth.register');
+        } else {
+            return view('user.profile');
+        }    
+    }
 }
--- marktplaats/routes/web.php
+++ marktplaats/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SessionController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/profile', [UserController::class, 'index']);
+Route::get('/login', [SessionController::class, 'create'])
+    ->name('login');
+
+
+    
