c249f2937247c823afd153c60ef76b7bc60e97dd
elsem1 <ikke_elwin@hotmail.com>
2025-02-05 15:42:55 +0100

    Finished Componenten

---
+++ Vue3/Componenten/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref, computed } from 'vue'
+import GroceriesTable from './components/GroceriesTable.vue'
+
+const list = ref([
+  ['Brood', '1.00', 1],
+  ['Broccoli', '0.99', 2],
+  ['Krentebollen', '1.20', 4],
+  ['Noten', '1.50', 0],
+])
+
+const headers = computed(() => ['Product', 'Prijs', 'Aantal', 'Subtotaal'])
+</script>
+
+<template>
+  <GroceriesTable :headers="headers" :list="list" />
+</template>
+
+<style scoped></style>
