d8b5c4c59e6fd3f0e705a1f5512d8aba12989902
elsem1 <ikke_elwin@hotmail.com>
2025-06-17 16:01:25 +0200

    Started an example-app as a base template

--- Vue3/example-app/resources/views/welcome.blade.php
+++ Vue3/example-app/resources/views/welcome.blade.php
@@ -4,18 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>example-app</title>
-
-        <!-- Fonts -->
-
-
-        <!-- Styles / Scripts -->
+        <title>example-app</title>        
         @vite(['resources/css/app.css', 'resources/js/app.ts'])
     </head>
-    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-        <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
-
-        </header>
-            <div id=app></div>
+    <body>
+        <div id=app></div>
+        
+            
     </body>
 </html>
